ec24b877fdafa7281b57c24f9408d0602828f8c8
ramirez-nathan <nater0826@gmail.com>
2025-11-15 19:08:01 -0800

    added functionality to event processor and incorporated trail

--- Assets/Scripts/GestureTrail.cs
+++ Assets/Scripts/GestureTrail.cs
@@ -3,72 +3,85 @@ using UnityEngine;
 
 public class GestureTrail : MonoBehaviour
 {
-    [Header("Drawing")]
-    public Transform drawTip;               // position at controller or finger
-    public LineRenderer lineRenderer;
-    public float minDistance = 0.015f;      // sensitivity for adding new points
+    [Header("Drawing - Right Hand")]
+    public Transform rightDrawTip;
+    public LineRenderer rightLineRenderer;
 
-    private bool isDrawing = false;
-    private List<Vector3> points = new List<Vector3>();
+    [Header("Drawing - Left Hand")]
+    public Transform leftDrawTip;
+    public LineRenderer leftLineRenderer;
+
+    [Header("Settings")]
+    public float minDistance = 0.015f;
+
+    private bool isDrawingRight = false;
+    private bool isDrawingLeft = false;
+
+    private List<Vector3> rightPoints = new List<Vector3>();
+    private List<Vector3> leftPoints = new List<Vector3>();
 
     void Update()
     {
-        // Detect trigger pull on the RIGHT controller (Quest Touch)
-        bool triggerDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
-        bool triggerUp   = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
+        // RIGHT HAND
+        bool rightTriggerDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
+        bool rightTriggerUp = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch);
 
-        // Start drawing
-        if (triggerDown)
+        // Only allow right hand to start if left hand isn't already drawing
+        if (rightTriggerDown && !isDrawingLeft)
         {
-            StartDrawing();
+            StartDrawing(ref isDrawingRight, rightPoints, rightLineRenderer);
+        }
+        if (rightTriggerUp)
+        {
+            StopDrawing(ref isDrawingRight, rightPoints, rightLineRenderer, true);
+        }
+        if (isDrawingRight)
+        {
+            AddPoint(rightDrawTip.position, rightPoints, rightLineRenderer);
         }
 
-        // Stop drawing
-        if (triggerUp)
-        {
-            StopDrawing();
-        }
+        // LEFT HAND
+        bool leftTriggerDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+        bool leftTriggerUp = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
 
-        // Record trail while drawing
-        if (isDrawing)
+        // Only allow left hand to start if right hand isn't already drawing
+        if (leftTriggerDown && !isDrawingRight)
         {
-            AddPoint();
+            StartDrawing(ref isDrawingLeft, leftPoints, leftLineRenderer);
+        }
+        if (leftTriggerUp)
+        {
+            StopDrawing(ref isDrawingLeft, leftPoints, leftLineRenderer, false);
+        }
+        if (isDrawingLeft)
+        {
+            AddPoint(leftDrawTip.position, leftPoints, leftLineRenderer);
         }
     }
 
-    void StartDrawing()
+    void StartDrawing(ref bool isDrawing, List<Vector3> points, LineRenderer lr)
     {
         isDrawing = true;
         points.Clear();
-        lineRenderer.positionCount = 0;
+        lr.positionCount = 0;
     }
 
-    void StopDrawing()
+    void StopDrawing(ref bool isDrawing, List<Vector3> points, LineRenderer lr, bool isRightHand)
     {
         isDrawing = false;
-
-        // Remove visual trail
-        lineRenderer.positionCount = 0;
-
-        // Send points to MiVRy
-        // MiVRyRecognizer.Instance.Recognize(points);
+        lr.positionCount = 0;
     }
 
-    void AddPoint()
+    void AddPoint(Vector3 tipPos, List<Vector3> points, LineRenderer lr)
     {
-        Vector3 tipPos = drawTip.position;
-
         if (points.Count == 0 || Vector3.Distance(points[^1], tipPos) > minDistance)
         {
             points.Add(tipPos);
-
-            lineRenderer.positionCount = points.Count;
-            lineRenderer.SetPosition(points.Count - 1, tipPos);
+            lr.positionCount = points.Count;
+            lr.SetPosition(points.Count - 1, tipPos);
         }
     }
 
-    public List<Vector3> GetPoints()
-    {
-        return points;
-    }
+    public List<Vector3> GetRightPoints() => rightPoints;
+    public List<Vector3> GetLeftPoints() => leftPoints;
 }
